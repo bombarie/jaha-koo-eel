d3eac9efea33eceab01e753a525415702c662a4a
Adriaan Wormgoor <adriaan@adriaanwormgoor.nl>
2023-12-15 16:06:17 +0100

    switch to NeoPixel library, add sinusoidal motor control

--- arduino/platformio/esp32c3_helloworld/esp32c3_helloworld/src/main_seeed_xiao_esp32c3.cpp
+++ arduino/platformio/esp32c3_helloworld/esp32c3_helloworld/src/main_seeed_xiao_esp32c3.cpp
@@ -3,6 +3,17 @@
 #include <Adafruit_BusIO_Register.h>
 #include "sbus.h"
 #include <HardwareSerial.h>
+#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
+
+// NeoPixel
+#define PIN D8      // On Trinket or Gemma, suggest changing this to 1
+#define NUMPIXELS 2 // Popular NeoPixel ring size
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 HardwareSerial MySerial0(0);
 
@@ -14,20 +25,20 @@ bfs::SbusData data;
 
 #define PIN_MOTOR1_A D5
 #define PIN_MOTOR1_B D4
-#define CH_MOTOR1_1 0 // 16 Channels (0-15) are availible
-#define CH_MOTOR1_2 1 // Make sure each pin is a different channel and not in use by other PWM devices (servos, LED's, etc)
+#define CH_MOTOR1_1 2 // 16 Channels (0-15) are availible
+#define CH_MOTOR1_2 3 // Make sure each pin is a different channel and not in use by other PWM devices (servos, LED's, etc)
 
 #define PIN_MOTOR2_A D3
 #define PIN_MOTOR2_B D2
-#define CH_MOTOR2_1 2 // 16 Channels (0-15) are availible
-#define CH_MOTOR2_2 3 // Make sure each pin is a different channel and not in use by other PWM devices (servos, LED's, etc)
+#define CH_MOTOR2_1 4 // 16 Channels (0-15) are availible
+#define CH_MOTOR2_2 5 // Make sure each pin is a different channel and not in use by other PWM devices (servos, LED's, etc)
 
 // Optional Parameters
 #define RES 8     // Resolution in bits:  8 (0-255),  12 (0-4095), or 16 (0-65535)
 #define FREQ 5000 // PWM Frequency in Hz
 
-MX1508 motorA(PIN_MOTOR1_A, PIN_MOTOR1_B, CH_MOTOR1_1, CH_MOTOR1_2); // Default-  8 bit resoluion at 2500 Hz
-MX1508 motorB(PIN_MOTOR2_A, PIN_MOTOR2_B, CH_MOTOR2_1, CH_MOTOR2_2); // Default-  8 bit resoluion at 2500 Hz
+MX1508 motorA(PIN_MOTOR1_A, PIN_MOTOR1_B, CH_MOTOR1_1, CH_MOTOR1_2, 8, 2500); // Default-  8 bit resoluion at 2500 Hz
+MX1508 motorB(PIN_MOTOR2_A, PIN_MOTOR2_B, CH_MOTOR2_1, CH_MOTOR2_2, 8, 2500); // Default-  8 bit resoluion at 2500 Hz
 
 u_long sbusPrevPacketTime;
 bool sbusLost = false;
@@ -37,14 +48,28 @@ bool sbusLost = false;
 #define SBUS_VAL_CENTER 988
 #define SBUS_VAL_DEADBAND 5
 #define SBUS_LOST_TIMEOUT 100
+#define SBUS_SWITCH_MIN 192
+#define SBUS_SWITCH_MAX 1792
+#define SBUS_SWITCH_MIN_THRESHOLD 1400
+#define SBUS_SWITCH_MAX_THRESHOLD 550
 
 #define TX_ROLL 0
 #define TX_PITCH 1
 #define TX_THROTTLE 2
 #define TX_YAW 3
+#define TX_AUX1 4
+#define TX_AUX2 5
+#define TX_AUX3 6
+#define TX_AUX4 7
 
 #define SBUS_PACKET_PRINT_INTERVAL 100 // ms
 u_long sbusPacketPrintPrevTime = 0;
+
+float sinCounter = 0.0;
+float sinCounterIncrement = 0.05;
+
+byte hVal = 0;
+byte vVal = 0;
 
 void setup()
 {
@@ -57,11 +82,15 @@ void setup()
   // by default, let's have the program assume sbus is lost
   sbusPrevPacketTime = -SBUS_LOST_TIMEOUT;
 
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+
   Serial.println("Hello World!");
 }
 
 void loop()
 {
+  // pixels.clear(); // Set all pixel colors to 'off'
+
   // read SBUS
   if (sbus_rx.Read())
   {
@@ -87,7 +116,7 @@ void loop()
         }
         Serial.println();
       }
-      
+
       sbusPacketPrintPrevTime = millis();
     }
   }
@@ -118,10 +147,45 @@ void loop()
   int16_t motor1Val = constrain(map(data.ch[TX_ROLL], SBUS_VAL_MIN, SBUS_VAL_MAX, -50, 50), -255, 255);
   int16_t motor2Val = constrain(map(data.ch[TX_PITCH], SBUS_VAL_MIN, SBUS_VAL_MAX, -50, 50), -255, 255);
 
+
+  // hVal = constrain(map(data.ch[TX_YAW], SBUS_VAL_MIN, SBUS_VAL_MAX, 0, 250), 0, 250);
+  hVal = constrain(map(data.ch[TX_YAW], SBUS_VAL_MIN, SBUS_VAL_MAX, 0, 250), 0, 250);
+  vVal = constrain(map(data.ch[TX_THROTTLE], SBUS_VAL_MIN, SBUS_VAL_MAX, 0, 225), 0, 225);
+
+  //*
+  // sets variable hVal according to position of tx_aux1
+  if (data.ch[TX_AUX1] < SBUS_SWITCH_MIN_THRESHOLD)
+  {
+    // Serial.println("lock in hue val");
+    hVal = 32;
+    // hVal = 32;
+  }
+  //*/
+
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    pixels.setPixelColor(i, pixels.Color(hVal, 0, 25 + vVal));
+  }
+  pixels.show(); // Send the updated pixel colors to the hardware.
+  // for (int i = 0; i < NUM_LEDS; i++)
+  // {
+  //   leds[i] = CHSV(hVal, 240, 25 + vVal);
+  // }
+  // FastLED.show();
+
   int16_t throttleVal = motor2Val;
   float mix = constrain(map(data.ch[TX_ROLL], SBUS_VAL_MIN, SBUS_VAL_MAX, 0, 1000), 0, 1000) / 1000.0;
   motor1Val = (throttleVal * (1.0 - mix)) * 2;
   motor2Val = (throttleVal * mix) * 2;
+
+  sinCounterIncrement = map(data.ch[TX_THROTTLE], SBUS_VAL_MIN, SBUS_VAL_MAX, 200, 1000) / 10000.0;
+  float sinMult1 = (sin(sinCounter) + 1.0);
+  float sinMult2 = (sin(sinCounter + HALF_PI) + 1.0);
+  sinCounter += sinCounterIncrement;
+  // Serial.println("sinCounterIncrement: " + String(sinCounterIncrement));
+
+  motor1Val *= sinMult1;
+  motor2Val *= sinMult2;
 
   // MOTOR 1
   if (abs(motor1Val) < SBUS_VAL_DEADBAND)
