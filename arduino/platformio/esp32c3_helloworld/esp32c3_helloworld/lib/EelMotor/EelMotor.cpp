a5b7cbd6abe7df0ae18f0786e954867f667c81fe
Adriaan Wormgoor <adriaan@adriaanwormgoor.nl>
2024-02-20 21:36:14 +0100

    Create EelMotor class (copy of ESP32MX1508)

---
+++ arduino/platformio/esp32c3_helloworld/esp32c3_helloworld/lib/EelMotor/EelMotor.cpp
@@ -0,0 +1,84 @@
+#include "EelMotor.h"
+
+EelMotor::EelMotor(uint8_t pinIN1, uint8_t pinIN2, uint8_t ledChan)
+{
+    EelMotor(pinIN1, pinIN2, ledChan, resolution, freq);
+}
+
+EelMotor::EelMotor(uint8_t pinIN1, uint8_t pinIN2, uint8_t ledChan, uint8_t resolution)
+{
+    this->resolution = resolution;
+
+    EelMotor(pinIN1, pinIN2, ledChan, resolution, freq);
+}
+
+EelMotor::EelMotor(uint8_t pinIN1, uint8_t pinIN2, uint8_t ledChan, uint8_t resolution, long freq)
+{
+    this->freq = freq;
+    this->resolution = resolution;
+
+    _pinIN1 = pinIN1;
+    _pinIN2 = pinIN2;
+    _ledChan = ledChan; // ESP32 LED Channel for PWM to Pin
+
+    pinMode(_pinIN1, OUTPUT);
+    pinMode(_pinIN2, OUTPUT);
+    ledcSetup(_ledChan, freq, resolution); // Setup channel at specified Hz with 8 (0-255), 12 (0-4095), or 16 (0-65535) bit resolution
+
+    if (resolution == 8)
+    {
+        _maxpwm = 255;
+    } // Sets a flag on the motor so the object knows the max pwm value
+    if (resolution == 12)
+    {
+        _maxpwm = 4095;
+    }
+    if (resolution == 16)
+    {
+        _maxpwm = 65535;
+    }
+
+    motorStop();
+}
+
+void EelMotor::motorStop()
+{
+    ledcDetachPin(_pinIN1);
+    ledcDetachPin(_pinIN2);
+    digitalWrite(_pinIN1, LOW);
+    digitalWrite(_pinIN2, LOW);
+}
+
+void EelMotor::motorBrake()
+{
+    ledcDetachPin(_pinIN1);
+    ledcDetachPin(_pinIN2);
+    digitalWrite(_pinIN1, HIGH);
+    digitalWrite(_pinIN2, HIGH);
+}
+
+void EelMotor::motorGo(long pwmSpeed)
+{
+    _pwmVal = pwmSpeed;
+
+    ledcDetachPin(_pinIN2);
+    digitalWrite(_pinIN2, LOW);
+
+    ledcAttachPin(_pinIN1, _ledChan);
+    ledcWrite(_pinIN1, _pwmVal);
+
+    ledcWrite(_ledChan, _pwmVal);
+}
+
+void EelMotor::motorRev(long pwmSpeed)
+{
+    _pwmVal = pwmSpeed;
+
+    ledcDetachPin(_pinIN1);
+    digitalWrite(_pinIN1, LOW);
+
+    ledcAttachPin(_pinIN2, _ledChan);
+    ledcWrite(_pinIN2, _pwmVal);
+
+    ledcWrite(_ledChan, _pwmVal);
+}
---
+++ arduino/platformio/esp32c3_helloworld/esp32c3_helloworld/lib/EelMotor/EelMotor.h
@@ -0,0 +1,27 @@
+#ifndef ESP32EELMOTOR_h
+#define ESP32EELMOTOR_h
+
+#include "Arduino.h"
+
+class EelMotor
+{
+public:
+    EelMotor(uint8_t pinIN1, uint8_t pinIN2, uint8_t ledChan);
+    EelMotor(uint8_t pinIN1, uint8_t pinIN2, uint8_t ledChan, uint8_t resolution);
+    EelMotor(uint8_t pinIN1, uint8_t pinIN2, uint8_t ledChan, uint8_t resolution, long freq);
+    void motorGo(long pwmVal);
+    void motorRev(long pwmVal);
+    void motorStop();  // Sets both signals to low to allow motor to spin down
+    void motorBrake(); // Sets boths signals to high to hard brake the motor
+
+private:
+    uint8_t _pinIN1;        // Pin 1 to MX1508
+    uint8_t _pinIN2;        // Pin 2 to MX1508
+    uint8_t _ledChan;       // ESP32 ledc Channel for PWM
+    uint8_t resolution = 8; // PWM Resolution
+    long freq = 2500;       // PWM Freq
+    long _pwmVal;           // PWM Value (speed)
+    long _maxpwm;           // Max PWM Value of the Motor
+};
+
+#endif
